04c99bc0d7cb0f1e3242d72b1bde6cff236a856a
DeJa <barclay.deja@gmail.com>
2020-07-01 23:42:17 -0500

    Updates

---
+++ precache-manifest.bc5083342e75e060c4092ce8ce401523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8d4acb5a22575f2c0b1d86cd454398",
+    "url": "/Recipe-Box/index.html"
+  },
+  {
+    "revision": "88b281f870b501cc87ed",
+    "url": "/Recipe-Box/static/css/main.5128f2bb.chunk.css"
+  },
+  {
+    "revision": "a5f1941c472f65889291",
+    "url": "/Recipe-Box/static/js/2.5d2be124.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-Box/static/js/2.5d2be124.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88b281f870b501cc87ed",
+    "url": "/Recipe-Box/static/js/main.57865e5d.chunk.js"
+  },
+  {
+    "revision": "b04c4c39dbdc23934516",
+    "url": "/Recipe-Box/static/js/runtime-main.ed0c14b5.js"
+  }
+]);
